0157c262095fb29258eece09ba2b4d6ecc2b1f53
Priscilla Penate <pypenate@gmail.com>
2018-01-24 13:08:52 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,17 +1,20 @@
-
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1])
+var checkForMatch = function () {
+	if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1])
 {
 	alert("You found a match!");
 }else {
 	alert("Sorry, try again.");
 }
+}
 
+var flipCard = function (cardId) {
+cardsInPlay.push(cards[cardId]);
+checkForMatch();
+console.log("User flipped " + cards[cardId]);
+};
+
+
+flipCard(0);
+flipCard(2);
